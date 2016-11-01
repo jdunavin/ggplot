3055ebeab3ca059822a3723514d6e2f765b8c12b
jdunavin <jdunavin@gmail.com>
2016-10-31 19:21:29 -0500

    Added some chapter 5 exercises

--- gg5.R
+++ gg5.R
@@ -35,7 +35,29 @@ ggplot(mpg, aes(displ, hwy)) +
 class <- mpg %>% group_by(class) %>% summarize(n=n(), hwy=mean(hwy))
 
 # Recreate the graph in the book - This oughta do it
-p + 
-  geom_jitter(data=mpg, aes(class, hwy), width=0.2) +
-  geom_point(color="magenta", size=4) +
-  geom_text(aes(y=10, label=paste0("n=",n)), size=3, data=class)
+p <- ggplot(mpg, aes(class, hwy))
+p <- p + geom_jitter(data = mpg, aes(class, hwy), width = 0.2) + geom_point(data = class, color = 'red', size = 4) +
+    geom_text(aes(y=10, label=paste0("n=",n)), size=3, data=class)
+p
+
+### Exercise 5431
+ggplot(mpg) + geom_point(aes(mpg$displ, mpg$hwy))
+ggplot(data = mpg, aes(displ, hwy)) + geom_point()
+
+ggplot() +
+    geom_point(mapping = aes(y = hwy, x = cty), data = mpg) +
+    geom_smooth(data = mpg, mapping = aes(cty, hwy)) + ggtitle("Before")
+ggplot(data = mpg, aes(cty, hwy)) + geom_point() + geom_smooth() + ggtitle("After")
+
+ggplot(diamonds, aes(carat, price)) +
+    geom_point(aes(log(brainwt), log(bodywt)), data=msleep)
+# These two plots should not be combined, that is dumb    
+ggplot(diamonds, aes(carat, price)) + geom_point()
+ggplot(msleep, aes(log(brainwt), log(bodywt))) + geom_point()
+
+### Exercise 5432 - Does this work or make sense?
+ggplot(mpg) + geom_point(aes(class, cty)) + geom_boxplot(aes(trans, hwy))
+# It works in that it doesn't generate errors, but it doesn't make much sense.
+# This should be shown in two separate graphs.
+
+### Exercise 5433 - x is continuous in one layer and categorical in another
